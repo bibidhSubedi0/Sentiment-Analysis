535d3f74a1843b0a3a946109700fbe7ed5ab6fc5
AakrishtSP <akrisht62@gmail.com>
2025-04-18 23:42:05 +0545

    feat(RedditScraper): enhance logging and improve post collection efficiency

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import logging
+import time
 from src.Scraping.RedditScraper import RedditScraper
 from dotenv import dotenv_values  # type: ignore
 
 
 def main():
     config = dotenv_values(".env")
-    scraper = RedditScraper(client_id=config["CLIENT_ID"], client_secret=config["CLIENT_SECRET"],
-                            user_agent=config["USER_AGENT"], get_comments=False)
+    scraper = RedditScraper(
+        client_id=config["CLIENT_ID"],
+        client_secret=config["CLIENT_SECRET"],
+        user_agent=config["USER_AGENT"],
+        get_comments=False
+    )
+
+    start_time = time.time()
     logging.info("Starting scraping...")
-    data, filename = scraper.collect_posts("facebook", 100)
 
-    # find count of posts
-    count = 0
+    try:
+        data_facebook, filename_facebook = scraper.collect_posts("facebook", 100)
+        data_nvidia, filename_nvidia = scraper.collect_posts("nvidia", 100)
+        data_tesla, filename_tesla = scraper.collect_posts("tesla", 100)
+        data_Amd, filename_Amd = scraper.collect_posts("Amd", 100)
+        data_spacex, filename_spacex = scraper.collect_posts("spacex", 100)
 
-    count = len(data)
-    # else:
-    #     logging.info(f"Collected data from {len(data)} unique months")
-    #     for month in data:
-    #         count += len(month['posts'])
+    finally:
+        total_time = time.time() - start_time
+        # Convert to hours, minutes, seconds
+        hours, remainder = divmod(total_time, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        logging.info(
+            f"Total scraping time: {int(hours)}h {int(minutes)}m {seconds:.2f}s"
+        )
 
-    logging.info(f"Collected {count} posts")
 
 
 if __name__ == '__main__':
